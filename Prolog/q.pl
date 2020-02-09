10915cbce029b7f4e0a4f485d942b3c19336602d
Rishabh2504 <rishabh.jain2504>
2020-02-09 19:38:53 +0530

    commit

--- Prolog/q.pl
+++ Prolog/q.pl
@@ -1,8 +1,12 @@
 go:- 
-	rainy(a).
-	rainy(b).
-	cold(b).
-	snowy(X):- rainy(X) , ! , cold(X).
-	snowy(t).
 
-initialization(go).
+	 
+invented(edison,lightbulb).
+invented(colmeraurer,prolog).
+iq(einstein,210).
+iq(edison,160).
+iq(waldorf,90).
+
+smart_invention(Invention):-invented(Person,Invention),iq(Person,_).
+
+:-initialization(go).
