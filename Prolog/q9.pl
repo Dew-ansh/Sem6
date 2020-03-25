8558ca8190a00c78b7f6245537440756fbcd2de0
Rishabh2504 <rishabh.jain2504>
2020-03-25 14:06:38 +0530

    change

--- Prolog/q9.pl
+++ Prolog/q9.pl
@@ -4,11 +4,7 @@ edge(q, r).
 edge(q, s). 
 edge(s, t).
 
-go:-	
-    path(p,r),
-    write('Path exist between given nodes'),nl;
-    write('No Path exist between given nodes').
-
+go:-
     path(A, B):-    edge(A, B), !.
     path(A, B):-    edge(A, C),
                     path(C, B).
